9656492d89d38ec0f8f20f42d6675bcbcf5d6958
tamari tinsley <tamaritinsley16@gmail.com>
2018-12-04 13:09:42 -0800

    finished app

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-a
+// Get Input Element
+let filterInput = document.getElementById('filterInput');
+// Get Event Listener
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames() {
+    // get value of input
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+   
+    // get names ul
+    let ul = document.getElementById('names');
+    // get li's from ul
+    let li = ul.querySelectorAll('li.collection-item');
+    
+    // loop through collection items li's
+    
+    for (i = 0; i < li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        // if matched
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+            }
+    }
+}
